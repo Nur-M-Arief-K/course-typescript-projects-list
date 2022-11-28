f7f800e052e5aa14d3de323a00887dc2d9e71746
Nur-M-Arief-K <ariefnur141@gmail.com>
2022-11-28 20:08:03 +0700

    using a getter

--- dist/app.js
+++ dist/app.js
@@ -109,6 +109,16 @@ class Component {
 }
 ;
 class projectItem extends Component {
+    get persons() {
+        if (this.project.people === 1) {
+            return "1 person";
+        }
+        else {
+            return `${this.project.people} persons`;
+        }
+        ;
+    }
+    ;
     constructor(hostId, project) {
         super("single-project", hostId, false, project.id);
         this.project = project;
@@ -120,7 +130,7 @@ class projectItem extends Component {
     ;
     renderContent() {
         this.element.querySelector("h2").textContent = this.project.title;
-        this.element.querySelector("h3").textContent = this.project.people.toString();
+        this.element.querySelector("h3").textContent = this.persons + " assigned";
         this.element.querySelector("p").textContent = this.project.description;
     }
     ;
